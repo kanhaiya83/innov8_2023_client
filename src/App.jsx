ce8efe88ef136a4a484707d45821a5c761636591
codewdme <lll.rg3.lll@gmail.com>
2023-09-28 03:36:35 +0530

    added learn page basic css setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ import Playground from "./pages/Playground";
 import Learn from "./pages/Learn";
 import Navbar from "./components/Navigation/Navbar";
 import Headroom from "react-headroom";
-import Modal from "./components/Navigation/Modal";
 import { AnimatePresence } from "framer-motion";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
 const App = () => {
-  const [showModal, setShowModal] = useState(false);
-
   return (
     <div>
       <Router>
@@ -23,17 +20,11 @@ const App = () => {
             webkitTransition: "all 0.8s ease-in-out",
           }}
         >
-          <Navbar setShowModal={setShowModal} showModal={showModal} />
+          <Navbar />
         </Headroom>
-        <Modal showModal={showModal} setShowModal={setShowModal} />
         <AnimatePresence wait>
           <Routes>
-            <Route
-              path="/"
-              element={
-                <Homepage showModal={showModal} setShowModal={setShowModal} />
-              }
-            />
+            <Route path="/" element={<Homepage />} />
             <Route path="/playground" element={<Playground />} />
             <Route path="/learn" element={<Learn />} />
             <Route path="/profile/:id" element={<Profile />} />
--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Learn = () => {
   return (
-    <div>Learn</div>
-  )
-}
+    <div>
+      <div className="text-center text-5xl md:text-6xl lg:text-8xl font-bold">
+        Learn Now!
+      </div>
+      <div className="flex flex-col justify-center items-center p-10 lg:flex-row gap-8">
+        {/* Options */}
+        <div className="learning-option-card">
+          <div className="learning-option-card-image"></div>
+          <div className="w-full bg-transparent h-[20%] text-2xl pl-2 font-bold">
+            Alphabets
+          </div>{" "}
+          <Link className="learn-card-button " to="/">
+            <button className="">Start Now!</button>
+          </Link>
+        </div>
+        <div className="learning-option-card">
+          <div className="learning-option-card-image"></div>
+          <div className="w-full bg-transparent h-[20%] text-2xl pl-2 font-bold">
+            Numbers
+          </div>{" "}
+          <Link className="learn-card-button " to="/">
+            <button className="">Start Now!</button>
+          </Link>
+        </div>
+        <div className="learning-option-card">
+          <div className="learning-option-card-image"></div>
+          <div className="w-full bg-transparent h-[20%] text-2xl pl-2 font-bold">
+            Phrases
+          </div>{" "}
+          <Link className="learn-card-button " to="/">
+            <button className="">Start Now!</button>
+          </Link>
+        </div>
+        <div className="learning-option-card">
+          <div className="learning-option-card-image"></div>
+          <div className="w-full bg-transparent h-[20%] text-2xl pl-2 font-bold">
+            Quizzz
+          </div>{" "}
+          <Link className="learn-card-button " to="/">
+            <button className="">Start Now!</button>
+          </Link>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default Learn
+export default Learn;
